077e6a743dca05477a91c1cd5114546807499540
evann52 <ivansemenychev99@gmail.com>
2024-12-16 18:00:33 +0300

    Выполненное задание. Реализация классов main, Car, Race.

---
+++ src/main/java/Car.java
@@ -0,0 +1,20 @@
+public class Car {
+
+    String model;
+    int speed;
+
+    Car(String model, int speed) {
+
+        this.model = model;
+        this.speed = speed;
+
+    }
+
+    public String getModel() {
+        return model;
+    }
+
+    public int getSpeed() {
+        return speed;
+    }
+}
--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,6 +1,43 @@
+import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        Scanner scanner = new Scanner(System.in);
+
+        Race race = new Race();
+        Car[] cars = new Car[3];
+
+        for (int i = 0; i < 3; i++) {
+
+            System.out.print("Введите название автомобиля №" + (i + 1) + ": ");
+            String name = scanner.next();
+
+            int speed = 0;
+
+            while (true) {
+                System.out.print("Введите скорость автомобиля №" + (i + 1) + ": ");
+                speed = scanner.nextInt();
+                if (speed > 0 && speed <= 250) {
+                    break;
+                } else {
+                    System.out.println("Ошибка! Скорость должна быть больше 0 и меньше 251.");
+                }
+            }
+
+            cars[i] = new Car(name, speed);
+            race.checkLeader(cars[i]);
+        }
+
+        System.out.println("Самая быстрая машина: " + race.getLeader());
+        scanner.close();
     }
+
 }
+
+
+
+
+
+
+
